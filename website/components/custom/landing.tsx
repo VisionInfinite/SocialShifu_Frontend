69c5219a94a0b7a8be3659dc24295e312b559436
roopesh <vetcharoopesh@gmail.com>
2024-10-06 00:56:37 +0530

    Roopesh : More Changes

--- website/components/custom/landing.tsx
+++ website/components/custom/landing.tsx
@@ -1,18 +1,20 @@
 import Navbar from "@/components/custom/navbar";
-import Circlers from "@/components/custom/circle";
 import { Button } from "@/components/ui/button";
 export default function Landing() {
   return (
     <div className="max-w-7xl mx-auto ">
       <Navbar />
       <div className="flex flex-col w-[100%] justify-center items-center h-[60vh]">
-        {/* <Circlers /> */}
-        <p className="text-5xl  text-center font-semibold mb-5">
-          Videos? Our AI Does That!
-        </p>
-        <p className=" text-gray-600/50">
-          Exclusively Open !! Join the waitlist below
-        </p>
+        <div className="lg:mt-[20vh] mx-auto w-full">
+          <h1 className="text-5xl  text-center font-semibold mb-5">
+            #1 In the Market. Top-Notch Videos by AI.
+          </h1>
+          <h2 className="text-red-300 max-w-lg text-center">
+            SocialSifu Does all the video automation you need, from generating
+            to posting. Everything by us, just relax.
+          </h2>
+        </div>
+
         <div className="flex flex-col gap-3 items-center px-2 w-[50vw]rounded-xl ">
           <input
             className="focus:outline-none relative z-10 h-14 p-8 w-full rounded-md "
--- website/components/custom/navbar.tsx
+++ website/components/custom/navbar.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <>
-      <motion.nav className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-white text-black rounded-full px-6 py-3 my-3 flex items-center justify-between w-max lg:w-11/12 max-w-4xl z-50 border border-black">
+      <nav className="fixed top-8 left-1/2 transform -translate-x-1/2 bg-white text-black rounded-full px-6 py-3 my-3 flex items-center justify-between w-max lg:w-11/12 max-w-4xl z-50 border border-black">
         <div className="flex items-center gap-x-2 md:justify-start justify-center w-full md:w-auto">
           <div className="w-8 h-8 bg-white rounded-full">
             <img src="./logo.png" alt="" />
           </div>
-          <span className="font-semibold text-center">Jay</span>
+          <span className="font-semibold text-center">Social Shifu</span>
         </div>
         <div className="hidden lg:flex space-x-6">
           {["Work", "Benefits", "Pricing"].map((item) => (
@@ -22,7 +21,7 @@ const Navbar = () => {
             </a>
           ))}
         </div>
-      </motion.nav>
+      </nav>
     </>
   );
 };
