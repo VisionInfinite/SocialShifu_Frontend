f3477e2396321c3d8a512cb5751259541803b8e5
roopesh <vetcharoopesh@gmail.com>
2024-10-07 11:45:17 +0530

    Changes in file structure

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,8 +1,9 @@
 import Landing from "@/components/custom/Landing";
+import LandingPage from "@/components/custom/LandingPage";
 export default function Home() {
   return (
   <div>
-    <Landing />
+    <LandingPage />
   </div>
   );
 }
---
+++ website/components/custom/LandingPage.tsx
@@ -0,0 +1,23 @@
+import "@/components/custom/css/landing.sample.css";
+import { Hero } from "./Hero";
+import { HeroVideoShowcase } from "./HeroVideoShowcase";
+import VideoWork from "./VideoWork";
+import Navigation from "./Navigation";
+import FooterBranding from "./FooterBranding";
+export default function LandingPage() {
+  return (
+    <div className="mx-auto h-[100vh]">
+      <Navigation />
+      <Hero />
+      <div className="lg:mt-10">
+        <center>
+          <HeroVideoShowcase />
+        </center>
+        <div className="mt-20">
+          <VideoWork />
+        </div>
+      </div>
+      <FooterBranding />
+    </div>
+  );
+}
